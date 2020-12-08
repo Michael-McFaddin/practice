cec33bc10627c1ecec91ed470df16f367853068a
mike <mikemcfaddin@gmail.com>
2020-12-08 17:58:21 -0500

    worked on Object manipulation, js practice#4

--- basic_skills_practice4.js
+++ basic_skills_practice4.js
@@ -78,13 +78,16 @@
 // console.log(result);
 
 //--------------------------
+// https://reedbarger.com/how-to-transform-javascript-objects-the-power-of-objectkeys-values-entries/
 //  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 //     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
 // let people = {321: {name: "Alice", age: 31}, 322: {name: "Maria", age: 27}};
-// let newPeople = [];
-// newPeople = people.map(person => person);
-// console.log(newPeople);
-//--------------------------Look up JavaScript map nested objects
+// const peopleArr = Object.entries(people).reduce((acc, [id, person]) => {
+//   acc.push({ ...person, id });
+//   return acc;
+// }, []);
+// console.log(peopleArr);
+//--------------------------
 
 
 //  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
@@ -112,10 +115,39 @@
 // console.log(flattenObj(items));
 
 //  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
-//     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+//     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] 
+//     becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
 
 //  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
-//     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+//     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] 
+//     becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+let objs = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}];
+let authorsObj = {};
+
+// function createObj(obj) {
+//   for (let keys in obj) {
+//     if (authorsObj.hasOwnProperty(keys)) {
+//       authorsObj[keys].push(obj[keys]);
+//     } else {
+//       authorsObj[keys] = [obj[keys]];
+//     }
+//   }
+// }
+// console.log(createObj(objs));
+objs.forEach(book => {
+  // if (!authorsObj[book.author]) {
+  //   authorsObj[book.author] = [];
+  // } 
+  authorsObj[book.author] = [book.title];
+
+  // else {
+  //   authorsObj[book.author] = [book.title];
+  //   console.log("else");
+  // }
+  console.log(authorsObj);
+});
+// console.log(authorsObj);
+
 
 // 10. Given a hash, create a new hash that has the keys and values switched.
 //     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
