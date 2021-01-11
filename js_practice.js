f3eeb7fa8c401728bbe4b714f6e72010bdefa22b
mike <mikemcfaddin@gmail.com>
2021-01-11 15:06:40 -0500

    latest scrimba exercises

--- js_practice.js
+++ js_practice.js
@@ -24,23 +24,108 @@
 // input: [10, 20, 20, 10, 10, 30, 50, 10, 20]
 // output: 3 (pairs of socks)
 
-let pile = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+// let pile = [10, 20, 20, 10, 10, 30, 50, 10, 20];
 
-function sockMerchant(ar) {
-  let socks = {};
-  let pairCount = 0;
-  ar.forEach(sock => {
-    if (socks[sock]) {
-      socks[sock] += 1;
-    } else {
-      socks[sock] = 1;
-    }
-  });
-  for (let prop in socks) {
-    if (socks[prop] >= 2) {
-      pairCount += Math.floor(socks[prop] / 2);
-    }
-  }
-  return pairCount;
-}
-console.log(sockMerchant(pile));
+// function sockMerchant(ar) {
+//   let socks = {};
+//   let pairCount = 0;
+//   ar.forEach(sock => {
+//     if (socks[sock]) {
+//       socks[sock] += 1;
+//     } else {
+//       socks[sock] = 1;
+//     }
+//   });
+//   for (let prop in socks) {
+//     if (socks[prop] >= 2) {
+//       pairCount += Math.floor(socks[prop] / 2);
+//     }
+//   }
+//   return pairCount;
+// }
+// console.log(sockMerchant(pile));
+
+
+// const items2 = ['pencil', 'book','pencil'];
+
+// function findDuplicateInArray(array) {
+//   const count = {};
+//   const result = [];
+
+//   array.forEach(item => {
+//     if (count[item]) {
+//       count[item] += 1;
+//       return;
+//     }
+//     count[item] = 1;
+//   });
+
+//   for (let prop in count) { 
+//     if (count[prop] >= 2) {
+//       result.push(prop);
+//     }
+//   }
+
+//   console.log(count);
+//   return result;
+
+// }
+
+// findDuplicateInArray(items2);
+// {pencil: 2, book: 1}
+//[pencil
+//_______________________________________________________
+// https://www.google.com/search?q=hackerrank+repeated+string+javascript&oq=hackerrank&aqs=chrome.0.69i59l3j69i57j69i60l3j69i65.7509j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_jgb2X8GPN4qo5NoP6-aYmAs13
+// s = substring, n = length of final string made up of substring
+// s = 'aba'      n = 10
+// figure out how many times 'a' occurs in a string length of n
+// function repeatedString(s, n) {
+//   let numberOfAs = 0;
+
+//   // find out how many 'a's you can get out of the original substring
+//   let occurenceOfA = [...s].filter(letter => letter === 'a').length;
+
+//   // divide n by length of substring to find out how many times you can multiple
+//   // occurencesOfA by
+//   numberOfAs = Math.floor(n / s.length) * occurenceOfA;
+ 
+//   // rem will get the rmainder
+//   let rem = n % s.length;
+
+//   for (let i = 0; i < rem; i++) {
+//     if (s[i] === 'a') numberOfAs++;
+//   }
+//   return numberOfAs;
+// }
+  
+// console.log(repeatedString('aba', 10));
+//_______________________________________________________________
+//https://medium.com/@clarkjohnson_85334/yet-another-hackerrank-problem-2d-array-hourglass-in-javascript-162614dff390
+// let grid = [
+//   [1, 1, 1, 0, 0, 0],
+//   [0, 1, 0, 0, 0, 0],
+//   [1, 1, 1, 0, 0, 0],
+//   [0, 0, 2, 4, 4, 0],
+//   [0, 0, 0, 2, 0, 0],
+//   [0, 0, 1, 2, 4, 0]
+// ];
+
+// grid[0][0], grid[0][1], grid [0][2]
+//             grid[1][1]
+// grid[2][0], grid[2][1], grid[2][2]
+
+// function hourglassSum(arr) {
+//   let max = -63;
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       let sum = 0;
+//       sum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+//       max = max < sum ? sum : max;
+//     }
+//   }
+//   return max;
+// }
+// console.log(hourglassSum(grid));
+
+//__________________________________________________________________
+
--- scrimba.js
+++ scrimba.js
@@ -1,11 +1,165 @@
+// Day 1
 //input: ["abc", "ded"]
 //expected output: ["*****", "*abc*", "*ded*", "*****"]
-let arr = ["abc", "ded"];
+// let arr = ["abc", "ded"];
 
-function addBorder(array) {
-  let newArr = array.map(item => `*${item}*`);
-  newArr.unshift("*****");
-  newArr.push("*****");
-  return newArr;
+// function addBorder(array) {
+//   let newArr = array.map(item => `*${item}*`);
+//   newArr.unshift("*****");
+//   newArr.push("*****");
+//   return newArr;
+// }
+
+// Anwer
+// function addBorder(array) {
+//   const wall = '*'.repeat(array[0].length + 2);
+
+//   array.unshift(wall);
+//   array.push(wall);
+
+//   for (let i = 1; i < array.length - 1; i++) {
+//     array[i] = '*'.concat(array[i], '*');
+//   }
+
+//   return array;
+// }
+// console.log(addBorder(arr));
+//________________________________________________________________
+// Day 2
+// Take the two digits of the number below and find the sum of their combined values
+// let numb = 52;
+
+// function addTwoDigits(num) {
+//   let values = num.toString().split('');
+//   let sum = values.reduce((acc, current) => acc + (parseInt(current)), 0);
+//   return sum;
+//   // return [...words];
+// }
+// console.log(addTwoDigits(numb));
+
+// Day 3
+// Find the first duplicate number and return the index value of that second duplicate value. 
+//If there is no duplicate return -1.
+// input: [2, 1, 3, 5, 3, 3]
+// output: 3 (index)
+// input: [2, 4, 3, 5, 1]
+
+// let arr = [2, 1, 3, 5, 3, 3];
+// let arr2 = [2, 4, 3, 5, 1];
+
+// function firstDuplicate(nums) {
+//   let obj = {};
+//   let answer = -1;
+//   for (let i = 0; i < nums.length; i++) {
+//     !obj[nums[i]] ? obj[nums[i]] = 1 : answer = nums[i];
+//   }
+//   return answer;
+// }
+
+// function firstDuplicate(nums) {
+//   let obj = {};
+//   let answer = -1;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!obj[nums[i]]) {
+//       obj[nums[i]] = 1;
+//     }
+//     if (obj[nums[i]]) answer = nums[i];
+//   }
+//   return answer;
+// }
+
+// Answer
+// function firstDuplicate(nums) {
+//   const firstDup = {};
+//   // iterate nums
+//   for (let num of nums) {
+//     // check for a duplicated value
+//     if (firstDup.hasOwnProperty(num)) {
+//       // return duplicated nums
+//       return num;
+//     }
+  
+//     firstDup[num] = null;
+//   }
+  
+//   // dont find a duplicate return -1
+//   return -1;
+// }
+// console.log(firstDuplicate(arr2));
+//_______________________________________________________________
+
+// Day 3
+// Sum all the prime number up to and including the provided number
+// input: 10
+// output: 17
+//https://medium.com/@hugh.winchester/sum-all-primes-3e31589fdf6c
+
+// const val = 10;
+// sum of primes for 977 = 72179
+// function sumAllPrimes(num) {
+//   //get all the number up to num excluding 1 but including num
+//   let values = [];
+//   for (let i = 2; i <= num; i++) {
+//     values.push(i);
+//   }
+//   let sum = 0;
+//   //loop through those numbers and run a check to see if they are prime
+//   for (let i = 0; i < values.length; i++) {
+  
+//     let isPrime = true;
+//     for (let j = 2; j < values[i]; j++) {
+//       if (values[i] % j === 0) {
+//         isPrime = false;
+//       }
+//     }
+//   //if the number is prime added to a sum accumulator
+//     if (isPrime) sum += values[i];
+//   }
+//   return sum;
+ 
+// }
+// console.log(sumAllPrimes(val));
+
+//Scrimba answer
+// function sumAllPrimes(num) {
+//   let primesTotal = 0;
+  
+//   for (let i = 2; i < num; i++) {
+//     console.log(`outer, i = ${i}`);
+//     for (let j = 2; j <= i; j++) {
+//       console.log(`inner, j = ${j}`);
+//       if (i === j) {
+//         console.log(`${i} is prime`);
+//         primesTotal += i;
+//       }
+        
+//       if (i % j === 0) {
+//         break;
+//       }
+//     }
+//   }
+  
+//   return primesTotal;
+// }
+// console.log(sumAllPrimes(val));
+
+//___________________________________________________
+// Day 4
+const num = 248622;
+const num2 = 642386;
+
+function evenDigitsOnly(number) {
+  let numArr = (number.toString()).split('');
+  let isEven = false;
+  for (let i = 0; i < numArr.length; i++) {
+    if (parseInt(numArr[i]) % 2 !== 0) {
+      isEven = false;
+      break;
+    } else {
+      isEven = true;
+    }
+  }
+  return isEven;
 }
-console.log(addBorder(arr));
+
+console.log(evenDigitsOnly(num2));
